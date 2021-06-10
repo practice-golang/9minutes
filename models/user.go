6ae2d745b4515efe88e71f5713f01f38ed7dd74d
Practice Golang <practice.golang@gmail.com>
2021-06-10 13:49:22 +0900

    User

--- models/user.go
+++ models/user.go
@@ -5,19 +5,19 @@ import (
 	"gopkg.in/guregu/null.v4"
 )
 
-// User - User(basic-user) data
+// User - (Minimal) User data
 type User struct {
 	Idx      null.String `json:"idx" db:"IDX" goqu:"skipinsert,skipupdate"`
-	Id       null.String `json:"id" db:"ID"`
-	Password null.String `json:"Password" db:"PASSWORD"`
+	UserID   null.String `json:"user-id" db:"USER_ID"`
+	Password null.String `json:"password" db:"PASSWORD"`
 	Name     null.String `json:"name" db:"NAME"`
 	Email    null.String `json:"email" db:"EMAIL"`
 }
 
 // Token - JWT token
 type Token struct {
-	Id    string
-	Name  string
-	Admin string
+	Id      string
+	Name    string
+	IsAdmin string
 	jwt.StandardClaims
 }
