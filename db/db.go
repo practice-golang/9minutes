7777f913b7b14ce37e711fb891e714f56d65af37
Practice Golang <practice.golang@gmail.com>
2022-07-07 16:13:31 +0900

    test repairing...

--- db/db.go
+++ db/db.go
@@ -4,6 +4,7 @@ import (
 	"9minutes/model"
 	"database/sql"
 	"errors"
+	"fmt"
 )
 
 const (
@@ -111,7 +112,7 @@ func SetupDB() error {
 		}
 
 	default:
-		return errors.New("database type not supported")
+		return errors.New("database type not supported " + fmt.Sprint(Info.DatabaseType))
 	}
 
 	return nil
