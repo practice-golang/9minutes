39150a3314854379114d2bed70efa5329a78314b
Practice Golang <practice.golang@gmail.com>
2023-08-20 01:44:16 +0900

    comment - working

--- handler/comment.go
+++ handler/comment.go
@@ -4,8 +4,13 @@ import (
 	"9minutes/config"
 	"9minutes/internal/crud"
 	"9minutes/model"
+	"fmt"
+	"net/http"
 	"strconv"
+	"strings"
+	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"gopkg.in/guregu/null.v4"
 )
 
@@ -67,4 +72,99 @@ func GetCommentsList(boardCode string, postingIDX string, queries map[string]str
 	}
 
 	return comments, nil
+}
+
+func GetComments(c *fiber.Ctx) (err error) {
+	boardCode, queries := c.Params("board_code"), c.Queries()
+	postingIdx := c.Params("idx")
+	if err != nil {
+		return c.Status(http.StatusBadRequest).SendString(err.Error())
+	}
+
+	comments, err := GetCommentsList(boardCode, postingIdx, queries)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(http.StatusOK).JSON(comments)
+}
+
+func WriteComment(c *fiber.Ctx) error {
+	board := model.Board{BoardCode: null.StringFrom(c.Params("board_code"))}
+	comment := model.Comment{}
+
+	postingIdx, err := strconv.ParseInt(c.Params("posting_idx"), 0, 64)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).SendString(err.Error())
+	}
+	comment.BoardIdx = null.IntFrom(postingIdx)
+
+	board, err = crud.GetBoardByCode(board)
+	if err != nil {
+		return c.Status(http.StatusNotFound).SendString("board not found")
+	}
+
+	err = c.BodyParser(&comment)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).SendString(err.Error())
+	}
+
+	comment.AuthorIdx = null.IntFrom(-1)
+
+	// Check session
+	sess, err := store.Get(c)
+	useridInterface := sess.Get("idx")
+	if err == nil && useridInterface != nil {
+		userIDX, err := strconv.Atoi(useridInterface.(string))
+		if err == nil {
+			comment.AuthorIdx = null.IntFrom(int64(userIDX))
+		}
+	}
+
+	comment.Content = null.StringFrom(bm.Sanitize(comment.Content.String))
+	if comment.Content.String == "" {
+		return c.Status(http.StatusBadRequest).SendString("comment is empty")
+	}
+
+	now := time.Now().Format("20060102150405")
+	comment.RegDate = null.StringFrom(now)
+
+	err = crud.WriteComment(board, comment)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString(err.Error())
+	}
+
+	result := map[string]interface{}{
+		"result": "success",
+	}
+
+	return c.Status(http.StatusOK).JSON(result)
+}
+
+func DeleteComment(c *fiber.Ctx) error {
+	var board model.Board
+
+	uri := strings.Split(c.Context().URI().String(), "/")
+
+	code := uri[len(uri)-3]
+	board.BoardCode = null.StringFrom(code)
+
+	boardIdx, _ := strconv.Atoi(uri[len(uri)-2])
+	commentIdx, _ := strconv.Atoi(uri[len(uri)-1])
+
+	board, err := crud.GetBoardByCode(board)
+	if err != nil {
+		return c.Status(http.StatusNotFound).SendString("Board was not found")
+	}
+
+	err = crud.DeleteComment(board, fmt.Sprint(boardIdx), fmt.Sprint(commentIdx))
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString(err.Error())
+	}
+
+	result := map[string]interface{}{
+		"result": "success",
+	}
+
+	return c.Status(http.StatusOK).JSON(result)
 }
