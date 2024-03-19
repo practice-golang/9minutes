12b1ac31f8346bd21d35c84547ed7b1483d78bc0
practice-golang <practice.golang@gmail.com>
2024-03-19 14:26:52 +0900

    rebuild front

--- static/html/admin/_app/immutable/chunks/paths.a30ab53b.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1w32xjq)==null?void 0:s.base)??"/admin";var a;const t=((a=globalThis.__sveltekit_1w32xjq)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ static/html/admin/_app/immutable/chunks/table.fd22c535.js
@@ -0,0 +1,1 @@
+import{h as s}from"./singletons.cf51a1e9.js";function c(n){return(n==null?void 0:n.length)!==void 0?n:Array.from(n)}const f=s("invalidate_all");export{c as e,f as i};
